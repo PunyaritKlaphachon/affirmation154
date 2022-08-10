35997df300494f94a545724f671b513c07bcbdcd
PunyaritKlaphachon <63410154@go.buu.ac.th>
2022-08-10 16:53:12 +0700

    3rd commit

--- app/src/main/java/com/example/affirmations154/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations154/adapter/ItemAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.affirmations154.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -15,11 +16,14 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        TODO("Not yet implemented")
+        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
     }
 
     override fun getItemCount(): Int {
